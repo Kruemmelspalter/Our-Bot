0b4af6b53058795da13e931ded161675c9d1ea1b
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2019-08-31 09:34:36 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 const auth = require("./auth.json");
-const words = require("./words.json")
-const Discord = require('discord.js')
-const client = new Discord.Client()
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const request = require('request');
+const swearwords = require("./swearwords.js");
+const web-request = require("./web-request.js");
 
 client.on('ready', () => {
   console.log("Logged in as" + client.user.tag + "!");
 });
 
 client.on('message', msg => {
-  if msg.content.startsWith("!web-request"){
-    var keyword = msg.substr(13).split(" ");
-    var keyword_processed = keywords[0];
-    for(i = 1;i<keyword.length();i++){
-      keyword_processed = keyword_processed +"-" +keyword[i];
-    }
-    request('https://www.google.de/search?q=' + keyword, function(err, res, body) {
-       result ="x-y" ;//x-y durch result ersetzen
-      msg.reply(result);
-    });
-  }
+  swearwords.check(msg);
 });
 
-client.login('token');
+client.login(auth.token);
